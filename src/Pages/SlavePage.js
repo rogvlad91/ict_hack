9b8e87cb4c4c95ffdbf44276d35bfa77df3a191c
Slava Rogov <rogvlad91@gmail.com>
2021-12-19 14:58:07 +0300

    final

--- src/Pages/SlavePage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function SlavePage() {
-  return (
-    <div className="formCSS ">
-      <h1 className="header"> Oh, I'm fucking cumming </h1>
-    </div>
-  );
-}
